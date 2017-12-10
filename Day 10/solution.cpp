7cc0c62832fba16eac480eb0ea1083238e6a0f98
Ocete <ocetebt@hotmail.com>
2017-12-10 22:49:00 +0100

    Day 10 done

--- Day 10/solution.cpp
+++ Day 10/solution.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <stack>
 using namespace std;
 
 void ReadVector(vector<int> & v) {
@@ -31,28 +32,29 @@ void AdvanceIterator(list<int> & l, list<int>::iterator & it, int offset) {
   }
 }
 
-int Position(list<int> & l, list<int>::iterator & it) {
-  int pos = 0;
-  list<int>::iterator it_aux = l.begin();
-  while (it_aux != it) {
-    it_aux++;
-    pos++;
+// Reverse de elements [begin, end) from the list.
+void Reverse(list<int> & l, list<int>::iterator begin, list<int>::iterator end) {
+  stack<int> s;
+  list<int>::iterator it = begin;
+  while (it != end) {
+    s.push(*it);
+    it++;
+    if (it == l.end()) it = l.begin();
   }
-  return pos;
+  it = begin;
+  while (it != end) {
+    *it = s.top();
+    s.pop();
+    it++;
+    if (it == l.end()) it = l.begin();
+  }
 }
-
-void Rotate(list<int> & l) {
-  int aux = *( l.begin() );
-  l.pop_front();
-  l.push_back(aux);
-}
-
 
 int KnotHash() {
-  list<int> l, aux_list;
+  list<int> l;
   vector<int> lengths;
-  int skip_size = 0, size_of_list = 5;
-  list<int>::iterator current_pos, final_pos, it_aux;
+  int skip_size = 0, size_of_list = 256;
+  list<int>::iterator current_pos, final_pos;
 
   for (int i=0; i<size_of_list; i++) {
     l.push_back(i);
@@ -60,42 +62,20 @@ int KnotHash() {
   current_pos = l.begin();
   ReadVector(lengths);
 
-  cout << "\n\tBefore the storm:" << endl;
-  PrintList(l);
+  //cout << "\n\tBefore the storm:" << endl;
+  //PrintList(l);
+
   for (int i=0; i<lengths.size(); i++) {
-    if (lengths[i] == size_of_list) {
-      int i_objetive = Position(l, current_pos) - 1, i_current_pos;
-      final_pos = current_pos;
-      AdvanceIterator(l, final_pos, 1);
-      // Insert every element but the first one
-      aux_list.splice(aux_list.begin(), l, final_pos, current_pos );
-
-      aux_list.reverse();
-      l.splice(current_pos, aux_list);
-
-      i_current_pos = Position(l, current_pos);
-      while (i_objetive != i_current_pos) {
-        Rotate(l);
-        i_current_pos = (i_current_pos - 1) % size_of_list;
-      }
-
-      // Advance current_pos
-      AdvanceIterator(l, current_pos, skip_size+1);
-    } else {
+    if (lengths[i] > 0) {
       final_pos = current_pos;
       AdvanceIterator(l, final_pos, lengths[i]);
-      aux_list.splice( aux_list.begin(), l, current_pos, final_pos );
-      aux_list.reverse();
-      l.splice(final_pos, aux_list);
-
-      // Advance current_pos
-      current_pos = final_pos;
-      AdvanceIterator(l, current_pos, skip_size);
+      Reverse(l, current_pos, final_pos);
     }
+    AdvanceIterator(l, current_pos, lengths[i] + skip_size);
     skip_size++;
 
-      cout << "\n\tIteration:" << i << endl;
-      PrintList(l);
+    //cout << "\n\tIteration:" << i << endl;
+    //PrintList(l);
   }
   current_pos = l.begin();
   final_pos = current_pos;

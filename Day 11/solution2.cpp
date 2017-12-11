13b5f8551fa2e1c3740599cb913b995a397f606f
Ocete <ocetebt@hotmail.com>
2017-12-11 09:49:57 +0100

    Day 11 done

---
+++ Day 11/solution2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string GetMove() {
+  string str;
+  getline(cin, str, ',');
+  if (str[str.size()-1] == '\n')
+    str = str.substr(0,str.size()-1);
+  return str;
+}
+
+void Move(pair<int, int> & p, string str) {
+  if (str == "n") {
+    p.first += 2;
+  } else if (str == "nw") {
+    p.first++;
+    p.second--;
+  } else if (str == "ne") {
+    p.first++;
+    p.second++;
+  } else if (str == "s") {
+    p.first -= 2;
+  } else if (str == "sw") {
+    p.first--;
+    p.second--;
+  } else if (str == "se") {
+    p.first--;
+    p.second++;
+  } else {
+    cout << "This should never appear: " << str << '-' << endl;
+  }
+}
+
+int Dist(pair<int,int> & p) {
+  return abs(p.second) + abs( abs(p.first) - abs(p.second) ) / 2;
+}
+
+int HexPath() {
+  pair<int, int> current_pos (0,0);
+  string str;
+  str = GetMove();
+  int largestDist = 0, dist;
+  while (cin) {
+    Move(current_pos, str);
+    dist = Dist(current_pos);
+    if (dist > largestDist)
+      largestDist = dist;
+    str = GetMove();
+  }
+  return largestDist;
+}
+
+int main() {
+  cout << "\tSolucion: " << HexPath() << endl;
+}

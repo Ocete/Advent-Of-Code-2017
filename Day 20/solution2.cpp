d5aab61be46bafec34f43e91a72359312b83afd3
Ocete <ocetebt@hotmail.com>
2018-07-14 16:24:33 +0200

    Day 20 finish

--- Day 20/solution2.cpp
+++ Day 20/solution2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -48,23 +50,42 @@ void ReadSwarm (vector<particle> & swarm) {
   }
 }
 
+struct comp {
+  bool operator() (const vector<int> &v1, const vector<int> &v2) const {
+    return v1[0] > v2[0] || (v1[0] == v2[0] && v1[1] > v2[1]) ||
+          (v1[0] == v2[0] && v1[1] == v2[1] && v1[2] > v2[2]);
+  }
+};
+
 void AdvanceTime (vector<particle> & swarm) {
+  map<vector<int>, int, comp> positions;
+  map<vector<int>, int>::iterator it;
   vector<int> to_erase;
+
   for (int i=0; i<swarm.size(); i++) {
+    // Move particle
     for (int j=0; j<3; j++) {
       swarm[i].vel[j] += swarm[i].ac[j];
       swarm[i].pos[j] += swarm[i].vel[j];
     }
-    to_erase.clear();
-    for (int j=0; j<i; j++) {
-      if ( swarm[i].pos[0] == swarm[j].pos[0] &&
-           swarm[i].pos[1] == swarm[j].pos[1] &&
-           swarm[i].pos[2] == swarm[j].pos[2]) {
-        to_erase.push_back(j);
-      }
-    }
-    if (!to_erase.empty())
+
+    // Check collisions
+    it = positions.find(swarm[i].pos);
+
+    if (it != positions.end()) {
       to_erase.push_back(i);
+      if (it->second != -1) {
+        to_erase.push_back(it->second);
+        it->second = -1;
+      }
+    } else {
+      positions[ swarm[i].pos ] = i;
+    }
+  }
+
+  // Delete collisions
+  if (!to_erase.empty()) {
+    sort(to_erase.begin(), to_erase.end());
     for (int j=to_erase.size()-1; j>=0; j--) {
       swarm.erase(swarm.begin() + to_erase[j]);
     }
@@ -75,7 +96,7 @@ int ParticleSwarm() {
   vector<particle> swarm;
 
   ReadSwarm(swarm);
-  for (int i=0; i<1000000; i++)
+  for (int i=0; i<10000; i++)
     AdvanceTime(swarm);
 
   return swarm.size();
